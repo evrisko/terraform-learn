45fd8cf9953a124dc939945d6208a864ea8aba53
evrisko <euvrisko@gmail.com>
2020-03-03 09:38:21 +0300

    Update awslog.sh

--- aws/awslog.sh
+++ aws/awslog.sh
@@ -2,8 +2,6 @@
 echo "----------START--------------------"
 yum update -y
 yum install -y awslogs
-service awslogs start
 systemctl start awslogsd
-chkconfig awslogsd on
 systemctl enable awslogsd.service
 echo "----------FINISH-------------------"
